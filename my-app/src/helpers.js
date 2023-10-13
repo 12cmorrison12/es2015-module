a3804075a9ae4c0d11af9feb5d1a2add131989f6
Carver Morrison <12cmorrison12@gmail.com>
2023-10-12 18:49:36 -0600

    added helper functions

---
+++ my-app/src/helpers.js
@@ -0,0 +1,16 @@
+/**function to choose a random item from array */
+const randomChoice = (items) => {
+    let i = Math.floor(Math.random() * items.length)
+    return items[i];
+}
+
+/**returns a copy of the array without the item chosen*/
+const removeChoice = (item, items) => {
+    for(let i =0; i < items.length; i++) {
+        if(items[i] === item) {
+            return [...items.slice(0, i), ...items.slice(i +1)]
+        }
+    }
+};
+
+export {randomChoice, removeChoice};
